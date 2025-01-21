f005f54db7440d9380bd3ffd670b0d1060e0737e
Andriotis Dimitris <d.andriotis@orosimosoftware.gr>
2025-01-21 17:21:56 +0200

    create the particles

--- src/script.js
+++ src/script.js
@@ -18,15 +18,45 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const particleTexture = textureLoader.load('/textures/particles/3.png')
 
 /**
- * Test cube
+ * Particles
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
-scene.add(cube)
+// Geometry
+const particlesGeometry = new THREE.BufferGeometry()
+const count = 20000
+
+const positions = new Float32Array(count * 3) // Multiply by 3 because each position is composed of 3 values (x, y, z)
+const colors = new Float32Array(count * 3)
+
+for(let i = 0; i < count * 3; i++) // Multiply by 3 for same reason
+{
+    positions[i] = (Math.random() - 0.5) * 10 // Math.random() - 0.5 to have a random value between -0.5 and +0.5
+    colors[i] = Math.random()
+}
+
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3)) // Create the Three.js BufferAttribute and specify that each information is composed of 3 values
+particlesGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
+
+// Material
+const particlesMaterial = new THREE.PointsMaterial({
+    size: 0.1,
+    sizeAttenuation: true,
+    color: '#ff88cc', //mixes with the random colors
+
+    alphaMap: particleTexture,
+    transparent: true,
+    // alphaTest: 0.001,
+    // depthTest: false,
+    depthWrite: false,
+    blending: THREE.AdditiveBlending,
+    vertexColors: true
+})
+// Points
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(particles)
+
 
 /**
  * Sizes
@@ -81,6 +111,18 @@ const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
 
+    // Update particles
+    // particles.rotation.y = elapsedTime * 0.2 // could be used for galaxies
+    // particles.position.y = - elapsedTime * 0.02 // could be used for snowflakes
+    for(let i = 0; i < count; i++)
+        {
+            const i3 = i * 3
+
+            // this could create a could  dna effect
+            const x = particlesGeometry.attributes.position.array[i3]
+            particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x)
+        }
+        particlesGeometry.attributes.position.needsUpdate = true 
     // Update controls
     controls.update()
 
